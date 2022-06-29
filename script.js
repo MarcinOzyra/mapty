c6ad9220f2796f151ddaf61caad59d42bc23efbc
Marcin <ozi@post.pl>
2022-06-29 15:06:07 +0200

    added Leaflet map

--- script.js
+++ script.js
@@ -10,3 +10,29 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude, longitude } = position.coords;
+      //   console.log(`https://www.google.pt/maps/@${latitude},${longitude}`);
+      const coords = [latitude, longitude];
+      const map = L.map('map').setView(coords, 13); //13 - zoom
+
+      //   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      /*    
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      }).addTo(map);
+*/
+      L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
+        maxZoom: 18,
+        subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
+      }).addTo(map);
+      L.marker(coords).addTo(map).bindPopup('A pretty CSS3 popup.<br> Easily customizable.').openPopup();
+    },
+    function () {
+      alert('Could not get your position');
+    }
+  );
+}
